ea6abcb7df7c955b1dcea853469d4ae6475cf4f4
gustavovital <37124715+gustavovital@users.noreply.github.com>
2019-03-30 21:10:51 -0300

    Update gn0me2kde.sh

--- gn0me2kde.sh
+++ gn0me2kde.sh
@@ -8,42 +8,11 @@
 # Última data de modificação: 6/01/2019
 ##################################################################
 
-echo ""
-echo "====================================================================="
-echo "      Script de Instalação da Interface KDE e remoção do GNOME"
-echo "====================================================================="
-echo ""
-echo "  O procedimento a seguir irá instalar a interface gráfica KDE-Plasma"
-echo "  e removerá a interface gráfica do GNOME 3. ATENÇÂO: A MUDANÇA SERÁ "
-echo "  DEFINITIVA!"
-echo ""
-read -p "Deseja continuar? (y/N) " answer
 
-if [[ $answer = "y" ]]; then
-  apt-get install kde-full -y
-  apt-get apt-get install kde-standard -y
-  apt-get install kde-plasma-desktop -y
-  apt-get autoremove --purge gnome*
-  
-  echo "====================================================================="
-  echo "                      Instalação Concluida"
-  echo "====================================================================="
-  echo ""
-  echo ""
-  read -p "Deseja iniciar uma nova sessão agora? (y/N)" answer2
+apt-get install kde-full -y
+apt-get apt-get install kde-standard -y
+apt-get install kde-plasma-desktop -y
 
-  if [[ $answer2 = "y" ]]; then
-    echo "====================================================================="
-    echo "                     Iniciando Nova Sessão"
-    echo "====================================================================="
-    sleep 2
-    reboot
-  else
-    exit
-  fi
+echo "Removendo o gnome"
 
-else
-  echo "====================================================================="
-  echo "                      Instalação Cancelada"
-  echo "====================================================================="
-fi
+apt-get autoremove --purge gnome*
