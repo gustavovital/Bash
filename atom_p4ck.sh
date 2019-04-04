768473b7c6b21547bdaf03900e728f64fcaef782
gustavovital <37124715+gustavovital@users.noreply.github.com>
2019-04-04 15:16:18 -0300

    Create atom_p4ck.sh

---
+++ atom_p4ck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Instalação de ipykernels e user
+# para o funcionamento do atom-python
+#
+
+echo "RODAR O SCRIPT COMO ROOT"
+sleep 4
+
+# Atualizando o sistema:
+apt-get update && apt-get upgrade
+
+# Instalando o pip & pip3
+apt-get install python3-pip -y
+apt-get install python-pip -y
+
+# Instalando o ipykernel e ipykernel3
+python -m pip install ipykernel
+python3 -m pip install ipykernel
+
+pip install ipykernel
+pip3 install ipykernel
+
+# Instalando para usuários
+python -m pip install --user
+python3 -m pip install --user
+
+# Instalando os pacotes do atom:
+
+apm install hydrogen
+apm install hydrogen-launcher
+apm install file-icons
+apm install atom-clock
